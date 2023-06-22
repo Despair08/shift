2eaf0bc57fdf01e1073219c269c0debe54d36cdc
Despair08 <vodanila2008@ya.ru>
2023-06-22 12:34:34 +0300

    feat build app js

--- modules/burgerClickHandler.js
+++ modules/burgerClickHandler.js
@@ -3,7 +3,6 @@ import { openMobileNavMenu } from './openMobileNavMenu.js'
 const burgerBtn = document.querySelector('.burger__menu');
 
 const burgerClickHandler = () => {
-  console.log('it work')
     burgerBtn.addEventListener('click', openMobileNavMenu);
 }
 
