66157ee0077c782756f5864971434f98dbce3cc4
Despair08 <vodanila2008@ya.ru>
2023-06-22 12:37:45 +0300

    feat: gh-pages files

--- modules/menuItemsClickHandler.js
+++
@@ -1,8 +0,0 @@
-import { menu } from './openMobileNavMenu.js';
-import { burgerBtn } from './burgerClickHandler.js';
-
-export const menuItemsClickHandler = () => {
-  menu.classList.remove('header__nav--opened')
-  document.body.classList.remove('lock');
-  burgerBtn.classList.remove('burger__menu--clicked');
-}
