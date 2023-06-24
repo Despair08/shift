e9b2da32a88ada130edd194898d2074b2a1bf489
Despair08 <vodanila2008@ya.ru>
2023-06-24 12:32:28 +0300

    feat :finish version

--- modules/burger/burgerClickHandler.js
+++
@@ -1,9 +0,0 @@
-import { openMobileNavMenu } from './openMobileNavMenu.js'
-
-const burgerBtn = document.querySelector('.burger__menu');
-
-const burgerClickHandler = () => {
-    burgerBtn.addEventListener('click', openMobileNavMenu);
-}
-
-export { burgerClickHandler, burgerBtn}
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './app.js',
-  output: {
-    filename: 'prod.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
